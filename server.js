e575c837a4a5411b389511a0663aaf23f5994fea
Nagapriyankar <nagapriyankar@gmail.com>
2023-12-02 16:29:51 -0800

    day3 initial setup

--- server.js
+++ server.js
@@ -1,35 +1,9 @@
 const express = require('express')
 const app = express()
-//data
 
-let data = [
-    {
-        id: 1,
-        Name: "Priyanka",
-        age: 25
-    },
-    {
-        id: 1,
-        Name: "Ridhin",
-        age: 2
-    },
-    {
-        id: 1,
-        Name: "Dhnesh",
-        age: 26
-    }
-]
+//middleware
 
-//define endpoints
-//to get root route
-app.get('/', (request, response) => {
-    response.send('<h1>Hello World!  I Love India!<h1>');
-})
-
-//to get all the data
-app.get('/api/data', (request, response) => {
-    response.json(data)
-})
+app.use(express.json())
 
 //make server listen to the HTTP req
 const HOSTNAME = '127.0.0.1'
